b64426cc112a4fe6e846a4efe84729466a7cf94a
astepanov-od <mymailbooox@gmail.com>
2020-05-01 22:48:56 +0300

    tests added (under pressure by Bro)

--- src/Operations.java
+++ src/Operations.java
@@ -1,15 +1,18 @@
 public class Operations {
-    public static double plus (double numberOne, double numberTwo){
-        return numberOne+numberTwo;
+    public static double plus(double numberOne, double numberTwo) {
+        return numberOne + numberTwo;
     }
-    public static double minus (double numberOne, double numberTwo){
-        return numberOne-numberTwo;
+
+    public static double minus(double numberOne, double numberTwo) {
+        return numberOne - numberTwo;
     }
-    public static double multiplication (double numberOne, double numberTwo){
-        return numberOne*numberTwo;
+
+    public static double multiplication(double numberOne, double numberTwo) {
+        return numberOne * numberTwo;
     }
-    public static double division (double numberOne, double numberTwo){
-        return numberOne/numberTwo;
+
+    public static double division(double numberOne, double numberTwo) {
+        return numberOne / numberTwo;
     }
 
 }
