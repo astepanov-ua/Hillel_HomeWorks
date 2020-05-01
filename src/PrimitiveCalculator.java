d3bbe0af65d6a02df96c819b8c44111333faffc9
astepanov-od <mymailbooox@gmail.com>
2020-05-01 14:03:23 +0300

    minor text changes

--- src/PrimitiveCalculator.java
+++ src/PrimitiveCalculator.java
@@ -5,16 +5,16 @@ public class PrimitiveCalculator {
         System.out.print("Please input first number: ");
         Scanner scanner1 = new Scanner(System.in);
         double numberOne = scanner1.nextDouble();
-        System.out.println("Your input "+ numberOne +" as a first number");
+        System.out.println("You input "+ numberOne +" as a first number");
 
         System.out.print("Please input second number: ");
         Scanner scanner2 = new Scanner(System.in);
         double numberTwo = scanner2.nextDouble();
-        System.out.println("Your input "+ numberTwo +" as a second number");
+        System.out.println("You input "+ numberTwo +" as a second number");
 
-        System.out.println("Sum is " + (numberOne+numberTwo));
-        System.out.println("Diff is " + (numberOne-numberTwo));
-        System.out.println("Mult is " + (numberOne*numberTwo));
-        System.out.println("Div is " + (numberOne/numberTwo));
+        System.out.println("Sum  is: " + (numberOne+numberTwo));
+        System.out.println("Diff is: " + (numberOne-numberTwo));
+        System.out.println("Mult is: " + (numberOne*numberTwo));
+        System.out.println("Div  is: " + (numberOne/numberTwo));
     }
 }
