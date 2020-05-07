498820801886bc820432710de5df206d472c0528
astepanov-od <mymailbooox@gmail.com>
2020-05-07 08:23:33 +0300

    homework 2.1 done

--- src/PrimitiveCalculator.java
+++ src/PrimitiveCalculator.java
@@ -3,26 +3,56 @@ import java.util.Scanner;
 public class PrimitiveCalculator {
     public static void main(String[] args) {
         double numberOne, numberTwo;
+        boolean hasNotResult=false;
+        String alert = "Please, input correct data";
+        String firstNumber = "Please, input first number: ";
+        String secondNumber = "Please input second number: ";
+        System.out.println(firstNumber);
         Scanner scanner1 = new Scanner(System.in);
-        do {
-            System.out.println("Please input first number: ");
+        if (!scanner1.hasNext()) scanner1.next();
+        while (!scanner1.hasNextDouble()) {
+            System.out.println(alert);
             scanner1.next();
         }
-        while (!scanner1.hasNextDouble());
         numberOne = scanner1.nextDouble();
-
         System.out.println("You input " + numberOne + " as a first number");
 
-
-        System.out.print("Please input second number: ");
+        System.out.print(secondNumber);
         Scanner scanner2 = new Scanner(System.in);
+        while (!scanner2.hasNextDouble()) {
+            System.out.println(alert);
+            scanner2.next();
+        }
         numberTwo = scanner2.nextDouble();
         System.out.println("You input " + numberTwo + " as a second number");
-
-
-        System.out.println("Sum  is: " + (numberOne + numberTwo));
-        System.out.println("Diff is: " + (numberOne - numberTwo));
-        System.out.println("Mult is: " + (numberOne * numberTwo));
-        System.out.println("Div  is: " + (numberOne / numberTwo));
+        System.out.println("Input Math operation; ");
+        Scanner scanner3 = new Scanner(System.in);
+        do {
+            String operation = scanner3.next();
+            switch (operation) {
+                case "+": {
+                    System.out.println("Sum  is: " + (numberOne + numberTwo));
+                    break;
+                }
+                case "-": {
+                    System.out.println("Diff is: " + (numberOne - numberTwo));
+                    break;
+                }
+                case "*": {
+                    System.out.println("Mult is: " + (numberOne * numberTwo));
+                    break;
+                }
+                case "/": {
+                    System.out.println("Div  is: " + (numberOne / numberTwo));
+                    break;
+                }
+                default: {
+                    System.out.println(alert);
+                    hasNotResult=true;
+                }
+            }
+        }
+        while (hasNotResult);
+        System.out.println("Result is : ");
     }
 }
