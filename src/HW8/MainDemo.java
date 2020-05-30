83d4be2ac4cf71e17f4c817615ad50e5fda779c5
astepanov-od <mymailbooox@gmail.com>
2020-05-31 02:46:48 +0300

    HW8 is done without two methods: Object[] toArray(Object[] a) iterator()

---
+++ src/HW8/MainDemo.java
@@ -0,0 +1,68 @@
+package HW8;
+
+import HW6.Cat;
+import HW6.Dog;
+import HW6.GuideDog;
+
+public class MainDemo {
+    public static void main(String[] args) {
+        MyCollection myPets = new MyCollection();
+        myPets.toArray();
+        System.out.println("MyCollection size is : " + myPets.size());
+        System.out.println("is MyCollection empty? - " + myPets.isEmpty());
+        System.out.println("Add one element to MyCollection ");
+        myPets.add(new Dog("Billy"));
+        System.out.println("is MyCollection empty? - " + myPets.isEmpty());
+        System.out.println("Add two element to MyCollection ");
+        myPets.add(new Dog("Bonnie"));
+        myPets.add(new Cat("Clyde"));
+        myPets.add(new Cat("Tomas"));
+        System.out.println(myPets);
+        System.out.println("Is cat Clyde my pet? - " + myPets.contains(new Cat("Clyde")));
+        System.out.println("Is dog Clyde my pet? - " + myPets.contains(new Dog("Clyde")));
+        System.out.println("MyCollection size is : " + myPets.size());
+        myPets.remove(new Dog("Bonnie"));
+        System.out.println("Bonnie deleted");
+        System.out.println("Now I Have: " + myPets);
+        MyCollection removePets = new MyCollection();
+        removePets.add(new Cat("Kyle"));
+        removePets.add(new Dog("Billy"));
+        System.out.println("I want remove these pets: " + removePets);
+        myPets.removeAll(removePets);
+        System.out.println("Now I Have: " + myPets);
+        System.out.println("MyCollection size is : " + myPets.size());
+        System.out.println("Clear!");
+        myPets.clear();
+        System.out.println("Now I Have: " + myPets);
+        System.out.println("MyCollection size is : " + myPets.size());
+        MyCollection newPets = new MyCollection();
+        newPets.addAll(removePets);
+        System.out.println("Add few pets");
+        myPets.add(new GuideDog("Guidy", true));
+        myPets.addAll(newPets);
+        System.out.println(newPets);
+        myPets.add(new Cat("Zumba"));
+        myPets.add(new Cat("Kyle"));
+        System.out.println("Now I Have: " + myPets);
+        System.out.println("MyCollection size is : " + myPets.size());
+        MyCollection retainPets = new MyCollection();
+        retainPets.addAll(removePets);
+        System.out.println("I want retain these pets: " + retainPets);
+        myPets.retainAll(retainPets);
+        System.out.println("Now I Have: " + myPets);
+        System.out.println("MyCollection size is : " + myPets.size());
+        MyCollection containsPets = new MyCollection();
+        containsPets.addAll(myPets);
+        containsPets.remove(new Cat("Kyle"));
+        containsPets.add(new Dog("Bonnie"));
+        System.out.println("Add two element to MyCollection ");
+        myPets.add(new Dog("Bonnie"));
+        myPets.add(new Cat("Clyde"));
+        System.out.println("Now I Have: " + myPets);
+        System.out.println("MyCollection size is : " + myPets.size());
+        System.out.println("Do I have all these pets: " + containsPets + " - " + myPets.containsAll(containsPets));
+        containsPets.add(new Cat("Simon"));
+        System.out.println("Do I have all these pets: " + containsPets + " - " + myPets.containsAll(containsPets));
+        System.out.println("Thank You!");
+    }
+}
---
+++ src/HW8/MyCollection.java
@@ -0,0 +1,160 @@
+package HW8;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MyCollection implements Collection {
+    private Object[] data = new Object[10];
+    private int numberElements = 0;
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public int size() {
+        return numberElements;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (data == null || numberElements == 0);
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        if (!isEmpty()) {
+            for (Object datum : data) {
+                if (object != null && object.equals(datum)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[data.length];
+        for (int i = 0; i < data.length; i++) {
+            result[i] = data[i];
+        }
+        return result;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        checkLenght();
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null) {
+                data[i] = o;
+                if (o != null) {
+                    numberElements++;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != null && data[i].equals(object)) {
+                data[i] = null;
+                numberElements--;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        Object[] objects = c.toArray();
+        for (Object object : objects) {
+            add(object);
+        }
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        data = null;
+        numberElements = 0;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        Object[] objects = c.toArray();
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < c.size(); j++) {
+                if (data[i] != null && objects[j].equals(data[i])) {
+                    numberElements--;
+                    data[i] = null;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        Object[] objects = c.toArray();
+        for (Object o : objects) {
+            while (contains(o)) {
+                remove(o);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        Object[] objects = c.toArray();
+        for (Object o :
+                objects) {
+            if (o != null && !contains(o)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        if (data == null) {
+            return "[]";
+        }
+        for (Object datum : data) {
+            if (datum != null) {
+                result += datum + ", ";
+            }
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    private boolean isFull() {
+        return (data.length > 0 && data.length == numberElements);
+    }
+    private void checkLenght() {
+        if (isEmpty()) {
+            data = new Object[10];
+        }
+        if (isFull()) {
+            Object[] newData = new Object[data.length * 2];
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+    }
+
+
+}
