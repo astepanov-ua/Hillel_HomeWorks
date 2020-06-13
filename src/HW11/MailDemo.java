41c62cb440e3cf7a1b732bfbc1f92f57048d38a6
astepanov-od <mymailbooox@gmail.com>
2020-06-13 19:59:07 +0300

    HW11: 1st save

---
+++ src/HW11/MailDemo.java
@@ -0,0 +1,32 @@
+package HW11;
+
+import HW6.Dog;
+import HW9.FibonacciNumber;
+
+import java.util.Collections;
+
+
+public class MailDemo {
+    public static void main(String[] args) {
+        Object o1 = new Dog("Blackie");
+        Object o2 = 177;
+        Object o3 = "LOL!";
+        Object o4 = FibonacciNumber.getBySequenceNumber(15);
+        Object[] objects = {o1, o2, o3, o4};
+
+        MyArrayList arrayList = new MyArrayList(objects);
+        System.out.println("Test Iterator from MyArrayList:");
+        for (Object o : arrayList) {
+            System.out.println(o);
+        }
+
+        System.out.println();
+
+        MyStack stack = new MyStack(objects);
+        System.out.println("Test Iterator from MyStack:");
+        for (Object o : stack) {
+            System.out.println(o);
+        }
+
+    }
+}
