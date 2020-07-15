31051845fec8616c8eb8c3254547cc796d4fbee9
astepanov-od <mymailbooox@gmail.com>
2020-07-16 00:01:58 +0300

    HW19: complete. O(n)-notation

--- src/HW_19_Indexes_of_two_ints_equal_some_value/MainDemo.java
+++ src/HW_19_Indexes_of_two_ints_equal_some_value/MainDemo.java
@@ -1,40 +1,33 @@
 package HW_19_Indexes_of_two_ints_equal_some_value;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.TreeMap;
 
 public class MainDemo {
     public static void main(String[] args) {
-        int sum = 1520;
-        System.out.println(findIndexes(getListOfInts(sum),sum));
-        }
-
-    public static List getListOfInts (int size) {
-        List l = new ArrayList();
-        for (int i = 0; i < size; i++) {
-            l.add(2*i-2);//можно поставить любую логику добавления int
-        }
-        return l;
+        int sum = 150;
+        System.out.println(findIndexes(getListOfInts(sum), sum));
     }
 
-    //метод гарантирует возврат числа посредством TreeMap, т.е сложность не более log(n=list.size())
-    public static String findIndexes (List list, int sum) {
-        TreeMap map = new TreeMap();
+    public static List getListOfInts(int size) {
+        List list = new ArrayList();
+        for (int i = 0; i < size; i++) {
+            list.add(2 * i - 2);                   //можно поставить любую логику добавления int
+        }
+        return list;
+    }
+
+    public static String findIndexes(List list, int sum) {
+        HashMap map = new HashMap();
         for (int i = 0; i < list.size(); i++) {
             if (map.containsKey(list.get(i))) {
-                return "indexes: [" + map.get(list.get(i)) + ", " + i + "]"+"\n"+
-                       "values:  [" + list.get((int)map.get(list.get(i))) + ", " + list.get(i) + "]";
+                return "indexes: [" + map.get(list.get(i)) + ", " + i + "]" + "\n" +
+                        "values:  [" + list.get((int) map.get(list.get(i))) + ", " + list.get(i) + "]";
             } else {
                 map.put(sum - (int) list.get(i), i);
             }
         }
         return "[-1,-1]";
     }
-
-
-
-
-
-
 }
