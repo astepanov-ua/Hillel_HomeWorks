c51779d967399f8fc0d21060834395ab5397c152
astepanov-od <mymailbooox@gmail.com>
2020-08-09 19:59:47 +0300

    HW26: SimpleFunc 2.0

--- src/HW26_SimpleFunctions/Main.java
+++ src/HW26_SimpleFunctions/Main.java
@@ -8,15 +8,18 @@ public class Main {
         List<Double> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             list.add((i + 0.5) / (i - 0.5));
-        }
-        System.out.println(SimpleFunctions.avarage(list));
-
-        list.clear();
-        for (int i = 0; i < 20; i++) {
-            list.add((double) (Math.pow(-1, i + 1) * i));
-            //list.add((double) (Math.pow(-1, i) * i));
+            //list.add((double) i);
         }
         System.out.println(list.toString());
-        System.out.println(SimpleFunctions.method(list));
+        System.out.println(SimpleFunctions.avarage(list));
+        list.clear();
+
+        List<Integer> listInt = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            //listInt.add((int) (Math.pow(-1, i + 1) * i));
+            listInt.add((int) (Math.pow(-1, i) * i));
+        }
+        System.out.println(listInt.toString());
+        System.out.println(SimpleFunctions.sumOfPositiveAndEvenSquaredNumbers(listInt));
     }
 }
--- src/HW26_SimpleFunctions/SimpleFunctions.java
+++ src/HW26_SimpleFunctions/SimpleFunctions.java
@@ -5,27 +5,22 @@ import java.util.List;
 
 public class SimpleFunctions {
     static double avarage(List<Double> list) {
-        ArrayList<Double> numbers = (ArrayList<Double>) list;
-        int lastIndex = numbers.size() - 1;
-        while (numbers.get(1) != null) {
-            for (int i = 0; numbers.get(i) != null; i++) {
-                if (i != lastIndex - i) {
-                    numbers.set(i, numbers.get(i) + numbers.get(lastIndex - i));
-                    numbers.set(lastIndex - i, null);
-                }
-            }
-            lastIndex = list.indexOf(null) - 1;
-        }
-        return numbers.get(0) / numbers.size();
-    }
-
-    public static double method(List<Double> list) {
         double res = 0;
         ArrayList<Double> numbers = (ArrayList<Double>) list;
+        for (int i = 0; numbers.get(i) != null; i++) {
+            res += numbers.get(i);
+            if (i != numbers.size() - 1 - i) {
+                res += numbers.get(numbers.size() - 1 - i);
+                numbers.set(numbers.size() - 1 - i, null);
+            }
+        }
+        return res / numbers.size();
+    }
 
-        // можно было бы и без этой переменной, но громозко и нечитаемо, ИМХО
+    public static int sumOfPositiveAndEvenSquaredNumbers(List<Integer> list) {
+        int res = 0;
+        ArrayList<Integer> numbers = (ArrayList<Integer>) list;
         int lastIndex = numbers.size() - 1;
-
         for (int i = 0; numbers.get(i) != null; i++) {
             res += (numbers.get(i) % 2 != 1 && numbers.get(i) > 0) ?
                     numbers.get(i) * numbers.get(i) : 0;
