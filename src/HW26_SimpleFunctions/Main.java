062823de65c14929b537a4e5dd178062f7deb724
astepanov-od <mymailbooox@gmail.com>
2020-08-09 20:03:50 +0300

    HW26: SimpleFunc 2.1

--- src/HW26_SimpleFunctions/Main.java
+++ src/HW26_SimpleFunctions/Main.java
@@ -5,17 +5,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> list = new ArrayList<>();
+        List<Double> listDoubles = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            list.add((i + 0.5) / (i - 0.5));
-            //list.add((double) i);
+            // можно поменить местами комментирование строк и протестить
+
+            listDoubles.add((i + 0.5) / (i - 0.5));
+            //list.add((double) i); //можно протестить на очевидном примере
         }
-        System.out.println(list.toString());
-        System.out.println(SimpleFunctions.avarage(list));
-        list.clear();
+        System.out.println(listDoubles.toString());
+        System.out.println(SimpleFunctions.avarage(listDoubles));
+
 
         List<Integer> listInt = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
+            // можно поменить местами комментирование строк и протестить
+
             //listInt.add((int) (Math.pow(-1, i + 1) * i));
             listInt.add((int) (Math.pow(-1, i) * i));
         }
