00c97a89d50747ad6410edb48acfae44a225c3d5
astepanov-od <mymailbooox@gmail.com>
2020-09-22 10:26:21 +0300

    HW23: HelloServlet (override methods POST,GET,DELETE)

--- src/HW23_Tomcat_HelloWorld/HelloServlet.java
+++ src/HW23_Tomcat_HelloWorld/HelloServlet.java
@@ -9,10 +9,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        try (ServletOutputStream outputStream = resp.getOutputStream()) {
+//            outputStream.print("HelloWorld, from JavaServlet");
+//        }
+//    }
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (ServletOutputStream outputStream = resp.getOutputStream()) {
-            outputStream.print("HelloWorld, from JavaServlet");
+            outputStream.print("Get, from JavaServlet");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (ServletOutputStream outputStream = resp.getOutputStream()) {
+            String method = req.getMethod();
+            outputStream.print(method + " , from JavaServlet");
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (ServletOutputStream outputStream = resp.getOutputStream()) {
+            String method = req.getMethod();
+            outputStream.print(method + " , from JavaServlet");
         }
     }
 }
